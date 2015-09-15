a4c0daef3feca068c24992066852bbdfd8fd9a81
Mahesh Jadhav <mahesh430023@gmail.com>
2015-09-15 20:33:51 +0530

    custom map marker info window changes

--- app/src/main/java/com/example/maheshjadhav/events/MapActivity.java
+++ app/src/main/java/com/example/maheshjadhav/events/MapActivity.java
@@ -4,10 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -23,10 +30,57 @@ public class MapActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        showMapAndDirections();
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+        // Setting a custom info window adapter for the google map
+        map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            // Use default InfoWindow frame
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            // Defines the contents of the InfoWindow
+            @Override
+            public View getInfoContents(Marker arg0) {
+
+                // Getting view from the layout file info_window_layout
+                View v = getLayoutInflater().inflate(R.layout.info_window_layout, null);
+
+                // Getting the position from the marker
+                LatLng latLng = arg0.getPosition();
+
+                // Getting reference to the TextView to set latitude
+                TextView tvLat = (TextView) v.findViewById(R.id.event_address);
+
+                String eventAddress = "urvi park, Pokhran Road Number 2, subhash nagar, Thane West, Thane, Maharashtra 400606";
+                // Setting the address
+                tvLat.setText(eventAddress);
+
+                // Returning the view containing InfoWindow contents
+                return v;
+
+            }
+
+        });
+        showMapAndDirections(map);
     }
 
-    private void showMapAndDirections() {
+    private void showMapAndDirections(GoogleMap map) {
+        double lat = 19.2140596;
+        double lan = 72.9784518;
+        String eventName = "My Event";
+        String eventAddress = "urvi park, Pokhran Road Number 2, subhash nagar, Thane West, Thane, Maharashtra 400606";
+
+        if (map != null) {
+            showLocationMarker(lat, lan, eventName, eventAddress);
+            showMapApp();
+        }
+    }
+
+    private void showMapAndDirections2() {
         double lat = 19.2140596;
         double lan = 72.9784518;
         String eventName = "My Event";
